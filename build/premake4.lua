42e720406c4a5e50e21b1af4833ef007a4a44af8
Oleh Derevenko <oleh.derevenko@gmail.com>
2025-01-17 00:25:18 +0200

    Changed: Version bumped to 0.16.6

--- build/premake4.lua
+++ build/premake4.lua
@@ -4,7 +4,7 @@
 -- For more information on Premake: http://industriousone.com/premake
 ----------------------------------------------------------------------
 
-  ode_version = "0.16.5"
+  ode_version = "0.16.6"
 
 ----------------------------------------------------------------------
 -- Demo list: add/remove demos from here and the rest of the build
